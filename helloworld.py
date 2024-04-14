5f48dce11987221c40d3c79a1b1ae367b312adf7
Viet-Anh, Nguyen <146407338+vietanhnguyen742001@users.noreply.github.com>
2024-04-14 21:00:11 +0700

    Add files via upload
    1_Day_Introduction

---
+++ helloworld.py
@@ -0,0 +1,22 @@
+# Introduction
+# Day 1 - 30 DaysOfReviewPython Challenge
+
+print(2 + 3)    # addition(+)
+print(3 - 1)    # sucbtraction(-)
+print(2 * 3)    # multiplication(*)
+print(3 / 2)    # division(/)
+print(3 ** 2)   # exponential(**)
+print(3 % 2)    # modeulus(%)
+print(3 // 2)   # Floor division operator(//)
+
+# Checking data types
+
+print(type(10))                         # Int
+print(type(3.14))                       # Float
+print(type(1 + 3j))                     # Comlex
+print(type("Vanh"))                     # String
+print(type([1, 2, 3]))                  # List
+print(type({'name':'Vanh'}))            # Dictionary
+print(type({9.8, 3.14, 2.7}))           # Set
+print(type((9.8, 3.14, 2.7)))           # Tuple
+
